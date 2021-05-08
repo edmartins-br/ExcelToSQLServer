6c83813e373fff6e87939961a54b49f7d8f5a363
Eduardo Martins <edmartins_br@outlook.com>
2021-05-08 10:41:57 -0300

    name Fix

--- ExcelReader.py
+++ ExcelReader.py
@@ -14,7 +14,7 @@ print('Lendo arquivo Excel...')
 data = pd.read_excel(r'.\excel\ativos.xlsx')
 df = pd.DataFrame(data, columns = ['Month', 'Year', 'Local_ID',
                                    'Global_ID','Employee_name',
-                                   'Gender','Birthday','Age','fse_ise_k_ise',
+                                   'Gender','Birthday','Age','fs',
                                    'Hiring','Seniority','Staff_Operator',
                                    'Grade','Subgrade','Job_Family',
                                    'Career_level','Brazil_job_code','cbo',	
@@ -24,8 +24,8 @@ df = pd.DataFrame(data, columns = ['Month', 'Year', 'Local_ID',
                                    'Employment_type','Subsidiary',	
                                    'org_Product','Accounting_unit',	
                                    'Expense_type','org_Portuguese',	
-                                   'org_English','org_Code_GERP',	
-                                   'org_Name_GERP','org_Code_RHEV',	
+                                   'org_English','org_Code',	
+                                   'org_Name_GERP','org_Code',	
                                    'org_Name_RHEV','Location',	
                                    'Manager','Assistant','Realocated',	
                                    'Cont_Index','Index'])
@@ -35,7 +35,7 @@ print(df)
 print('Criando TABELA...')
 cursor.execute('''CREATE TABLE headCount ([Month] INT, [Year] INT, LocalID INT, Global_ID VARCHAR(50), 
                 EmployeeName VARCHAR(200), Gender VARCHAR(50), Birthday datetimeoffset(7),
-                Age int, fse_ise_k_ise VARCHAR(50),
+                Age int, fs VARCHAR(50),
                 Hiring datetimeoffset(7), Seniority int, Staff_Operator VARCHAR(50),
                 Grade VARCHAR(50), Subgrade VARCHAR(50), JobFamily VARCHAR(50),
                 CareerLevel VARCHAR(50), BrazilJobCode VARCHAR(50), cbo int,
@@ -43,8 +43,8 @@ cursor.execute('''CREATE TABLE headCount ([Month] INT, [Year] INT, LocalID INT, 
                 pcd VARCHAR(50), Status VARCHAR(50), StatusType VARCHAR(50), EmailAddres VARCHAR(200),
                 EmploymentType VARCHAR(50), Subsidiary NVARCHAR(50), OrgProduct VARCHAR(50),
                 AccounttingUnit VARCHAR(50), ExpenseType VARCHAR(50), OrgPortuguese VARCHAR(50),
-                OrgEnglish VARCHAR(50), OrgCodeGerp int, ORGNameGERP VARCHAR(50), 
-                ORGCodeRHEV VARCHAR(50), ORGNameRHEV NVARCHAR(50), [Location] NVARCHAR(50), Manager NVARCHAR(50), Assistant NVARCHAR(50),
+                OrgEnglish VARCHAR(50), OrgCode int, ORGName VARCHAR(50), 
+                ORGCode VARCHAR(50), ORGName NVARCHAR(50), [Location] NVARCHAR(50), Manager NVARCHAR(50), Assistant NVARCHAR(50),
                 Realocated VARCHAR(50), ContIndex VARCHAR(50), [Index] VARCHAR(50))''')
 
 print('Inserindo dados no banco banco...')
